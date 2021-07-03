32799ff55cd5828cbdb2f56dfdc7ce510cf42cc9
safeeadnan <72091073+safeeadnan@users.noreply.github.com>
2021-07-03 09:58:59 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ function updateTime(){
     let hour = currentTime.getHours();
     let minute = currentTime.getMinutes();
     let second = currentTime.getSeconds();
-
+    let date= currentTime.getDate()
+    let month= currentTime.getMonth()
+    let year= currentTime.getFullYear()
+    let day=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Saturday"]
     if (hour<10){
         hour="0"+hour;
     }
@@ -13,10 +16,20 @@ function updateTime(){
     if(second<10){
         second="0"+second;
     }
+    if (date<10){
+        date="0"+date;
+    }
+    if (month<10){
+        month="0"+month;
+    }
 
     document.getElementById("hour").innerHTML = hour +": ";
     document.getElementById("minute").innerHTML = minute +": ";
     document.getElementById("second").innerHTML = second;
+    document.getElementById("date").innerHTML = date+" /";
+    document.getElementById("month").innerHTML = month+" /";
+    document.getElementById("year").innerHTML = year;
+    document.getElementById("day").innerHTML = day[currentTime.getDay()-1];
 
 }
 setInterval(updateTime);
